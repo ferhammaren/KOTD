c4738ae05edd6ad5749c63504935a043ac138a52
ferhammaren <fernanda.meza.lopez@gmail.com>
2020-05-07 16:26:13 -0700

    read jmdict json spew random entry

---
+++ test.py
@@ -0,0 +1,13 @@
+import json
+import random
+#kanjiList
+kotd=[]
+with open('E:/Projects/KOTD/dict/JMDict.json',encoding="utf8") as f:
+    data=json.load(f)
+
+rndK=random.choice(data)
+kotd.append(rndK['k_ele'][0]['keb'][0])
+kotd.append(rndK['r_ele'][0]['reb'][0])
+kotd.append(rndK['sense'][0]['gloss'][0])
+
+
